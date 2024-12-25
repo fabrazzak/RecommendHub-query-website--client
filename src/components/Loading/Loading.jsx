23d858dd4801ec25cdeb418eeee2cef00aa241d5
fabrazzak <freelancerabdurrazzak47@gmail.com>
2024-12-26 04:11:13 +0600

    update

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "../../assets/loading.json";
 
 const Loading = () => {
     return (
         <div>
             <div className="flex justify-center items-center h-screen">
-                <span className="loading loading-infinity loading-lg"></span>
+                <Lottie className='h-72' animationData={groovyWalkAnimation} loop={true} />;
             </div>;
 
         </div>
